c765f7f4dcc7c5428a5b4e8d8b8417ada905131c
josieyao <34701731+josieyao@users.noreply.github.com>
2022-04-21 17:07:18 -0700

    nav links populate correct page in right container (#8)

--- src/templates/LeftNav/index.js
+++ src/templates/LeftNav/index.js
@@ -1,16 +1,56 @@
 import * as S from './styles';
+import { Link } from 'react-router-dom';
 
 export const LeftNav = () => {
   return (
     <S.LeftNavContainer>
       <S.LeftNavGreeting>Hello, Croissant</S.LeftNavGreeting>
       <S.LeftNavLinksContainer>
-        <S.LeftNavLink>Home</S.LeftNavLink>
-        <S.LeftNavLink>Results</S.LeftNavLink>
-        <S.LeftNavLink>Health Profile</S.LeftNavLink>
-        <S.LeftNavLink>Memberships</S.LeftNavLink>
-        <S.LeftNavLink>Orders</S.LeftNavLink>
-        <S.LeftNavLink>Account</S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link to="/" style={{ textDecoration: 'none', color: '#95c6bf' }}>
+            Home
+          </Link>
+        </S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link
+            to="/results"
+            style={{ textDecoration: 'none', color: '#95c6bf' }}
+          >
+            Results
+          </Link>
+        </S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link
+            to="/health-profile"
+            style={{ textDecoration: 'none', color: '#95c6bf' }}
+          >
+            Health Profile
+          </Link>
+        </S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link
+            to="/memberships"
+            style={{ textDecoration: 'none', color: '#95c6bf' }}
+          >
+            Memberships
+          </Link>
+        </S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link
+            to="/orders"
+            style={{ textDecoration: 'none', color: '#95c6bf' }}
+          >
+            Orders
+          </Link>
+        </S.LeftNavLink>
+        <S.LeftNavLink>
+          <Link
+            to="/settings"
+            style={{ textDecoration: 'none', color: '#95c6bf' }}
+          >
+            Account
+          </Link>
+        </S.LeftNavLink>
       </S.LeftNavLinksContainer>
     </S.LeftNavContainer>
   );
