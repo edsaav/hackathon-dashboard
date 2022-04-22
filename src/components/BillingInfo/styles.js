525ad8f7e746ae5e06adb197d40958ec5aa0b3a5
Josephine Yao <josiee.yao@gmail.com>
2022-04-22 09:27:28 -0700

    billing and subscriptions page done

---
+++ src/components/BillingInfo/styles.js
@@ -0,0 +1,26 @@
+import { Button } from '@everlywell/leaves';
+import styled from 'styled-components';
+
+export const StyledButton = styled(Button)`
+  margin-right: 20px;
+  width: 200px;
+`;
+
+export const CardsOnFileImage = styled.img`
+  max-width: 90%;
+  padding-bottom: 30px;
+`;
+
+export const SubscriptionsImage = styled.img`
+  max-width: 90%;
+  padding-bottom: 30px;
+`;
+
+export const BillingInfoTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const AddNewCardButtonContainer = styled.div`
+  margin-top: 25px;
+`;
